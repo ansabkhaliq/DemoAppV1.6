b7b9cc9dfbf1ac356b6bd10a526308966b244fe0
Muhammad Ansab Khaliq <mkhaliq@student.unimelb.edu.au>
2020-10-09 13:58:22 +1100

    Initial commit

---
+++ DemoAppV1.6/ViewModel/ViewModelBase.cs
@@ -0,0 +1,23 @@
+﻿using DemoAppV1._6.MVVM;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoAppV1._6.ViewModel
+{
+    internal class ViewModelBase : Observable
+    {
+        private static readonly ObservableCollection<MenuItem> AppMenu = new ObservableCollection<MenuItem>();
+
+        public ViewModelBase()
+        {
+        }
+
+        public ObservableCollection<MenuItem> Menu => AppMenu;
+
+      
+    }
+}
